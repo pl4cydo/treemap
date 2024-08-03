0fc24503ade7fe75494df6217b4eb5175492be1f
pl4cydo <placydo.doc@gmail.com>
2024-08-03 18:21:02 -0300

    feature: Changes the visualiation of treemap

--- src/Treemap.vue
+++ src/Treemap.vue
@@ -1,43 +1,89 @@
 <script setup lang="ts">
-import * as d3 from 'd3';
-import { onMounted } from 'vue';
+import { onMounted, ref, type Ref } from 'vue';
 
 export interface DataInterface {
   name: string,
   value: number
 }
 
+type RectangleType = {
+  top: string, 
+  left: string, 
+  height: string, 
+  width: string, 
+  backgroundColor: string,
+  border: string,
+  position: string,
+  display?: string
+  name?: string,
+  value?: number
+};
+
+interface TreemapDataInterface {
+  squere: RectangleType,
+  rectangles: RectangleType[]
+}
+
+interface boundsInterface { 
+  startTop: number, 
+  startLeft: number, 
+  right: number, 
+  bottom: number 
+};
+
 const props = defineProps<{
     data: { name: string, value: number }[],
     title: string
 }>()
 
+let treemapData: Ref<TreemapDataInterface> = ref({
+    squere: { 
+      top: '0px', 
+      left: '0px', 
+      height: '0px', 
+      width: '0px', 
+      backgroundColor: 'white',
+      border: '1px solid black',
+      position: 'relative',
+    },
+    rectangles: []
+  })
+
 function treemap(data: DataInterface[]): void {
   // organiza o array
   data.sort((a, b) => b.value - a.value)
   //soma os valores do array
-  const sum = data.reduce((s, i) => s + i.value, 0);
+  const sum: number = data.reduce((s, i) => s + i.value, 0);
   // declara o retangulo
-  const svg = d3.select('svg');
-  const width = parseInt(svg.attr('width')!);
-  const height = parseInt(svg.attr('height')!);
+  const width: number = 500;
+  const height: number = 500;
   // salva as coordenadas do espaço vazio
-  const bounds = { startTop: 0, startLeft: 0, right: width, bottom: height };
+  const bounds: boundsInterface = { startTop: 0, startLeft: 0, right: width, bottom: height };
 
   // quanto falta dos valores 
-  let weightLeft = sum;
+  let weightLeft: number = sum;
   // declar os eixos e tamanhos do retangulo a ser gerado
-  let eixoX, eixoY, rectangleWidth, rectangleHeigth;
+  let eixoX: number, eixoY: number, rectangleWidth: number, rectangleHeigth: number;
 
   // declara a cor inical retangulo
-  let color1 = 81;
-  let color2 = 81;
-  let color3 = 250;
+  let color1: number = 0;
+  let color2: number = 170;
+  let color3: number = 0;
+
+  treemapData.value.squere = { 
+      top: `${bounds.startTop}px`, 
+      left: `${bounds.startLeft}px`, 
+      height: `${bounds.bottom}px`, 
+      width: `${bounds.right}px`, 
+      backgroundColor: `white`,
+      border: '1px solid black',
+      position: 'relative',
+  },
 
   // itera pelo array de numeros e gera o retangulo
   data.forEach(el => {
-    const horizontalVoidSpace = bounds.right - bounds.startLeft;
-    const verticalVoidSpace = bounds.bottom - bounds.startTop;
+    const horizontalVoidSpace: number = bounds.right - bounds.startLeft;
+    const verticalVoidSpace: number = bounds.bottom - bounds.startTop;
     // const area = el / unit;
     eixoX = bounds.startLeft;
     eixoY = bounds.startTop;
@@ -54,26 +100,23 @@ function treemap(data: DataInterface[]): void {
     }
 
     weightLeft -= el.value;
-    svg.append('rect')
-      .attr('x', eixoX)
-      .attr('y', eixoY)
-      .attr('width', rectangleWidth)
-      .attr('height', rectangleHeigth)
-      .style('fill', `rgb(${color1}, ${color2}, ${color3})`)
-      .style('stroke', 'white')
-      .style('stroke-width', 3);
-    svg.append('text')
-      .text(`${el.name} - ${el.value}`)
-      .attr('x', eixoX + rectangleWidth / 2)
-      .attr('y', eixoY + rectangleHeigth / 2)
-      .attr('text-anchor', 'middle')
-      .attr('alignment-baseline', 'middle')
-      .style('fill', 'white')
-      .style('shadow', 'black');
 
-    color1+= 15;
-    color2+= 15;
+    treemapData.value.rectangles.push({ 
+      top: `${eixoY}px`,
+      left: `${eixoX}px`, 
+      height: `${rectangleHeigth}px`, 
+      width: `${rectangleWidth}px`,
+      backgroundColor: `rgb(${color1}, ${color2}, ${color3})`,
+      border: '1px solid white',
+      position: 'absolute',
+      name: el.name,
+      value: el.value
+    })
+
+    color1+= 10;
+    color2+= 10;
   });
+
 }
 
 onMounted(() => {
@@ -83,39 +126,35 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="block">
-      <div>
-        <h2>{{ props.title }}</h2>
-        <svg width="400" height="400"></svg>
-      </div>
-      <div class="side">
-        <h4>Lista: </h4>
-        <span v-for="item in props.data">
-            {{ item.name }} {{ item.value }}
-        </span>
-      </div>
+    <div style="height: 100vh; width: 100vw;">
+        <div :style="treemapData.squere">
+          <div 
+            v-for="rectangle in treemapData.rectangles" 
+            :style="{
+              ...rectangle, 
+              display: 'flex', 
+              alignItems: 'center', 
+              justifyContent: 'center', 
+              overflow: 'hidden',
+              flexDirection: 'column'
+              }">
+              <span>{{ rectangle.name }}</span>
+              <span>{{ rectangle.value }}</span>
+          </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
 
-.block {
-    padding: 10px;
-    border: 1px solid black;
-    display: flex;
+.rectangle {
+  display: flex;
+  text-align: center;
 }
 
-.side {
-    border: 1px solid black;
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    padding-right: 50px;
-    margin: 5px;
-}
 
 span {
-    padding-bottom: 5px;
+  color: rgb(255, 255, 255);
 }
 
 </style>
